ea59d32ed4341c466e03663e2bb2b828752c5698
HerviaultAxel <aherviault23@ubishops.ca>
2025-03-22 15:49:59 -0400

    Prefabs
    Added Prefabs to implement the game easily

--- Assets/Axel's Scene/New Game (Zombie Tag)/Scripts/Spawner.cs
+++ Assets/Axel's Scene/New Game (Zombie Tag)/Scripts/Spawner.cs
@@ -5,6 +5,11 @@ public class Spawner : MonoBehaviour
     public GameObject prefabToSpawn;  // Assign the prefab in the Inspector
     public GameObject BomberPrefab;  // Assign the prefab in the Inspector
     public GameObject CurlingPrefab;  // Assign the prefab in the Inspector
+
+    public float RegularProportion = 0.8f;
+    public float BomberProportion = 0.1f;
+    public float CurlingProportion = 0.1f;
+
     public float spawnInterval = 2f;  // Time between spawns
     public Vector2 minBounds = new Vector2(-5f, -5f);  // Bottom-left corner
     public Vector2 maxBounds = new Vector2(5f, 5f);    // Top-right corner
@@ -26,7 +31,17 @@ public class Spawner : MonoBehaviour
             Random.Range(minBounds.y, maxBounds.y)
         );
 
-        Instantiate(prefabToSpawn, randomPosition, Quaternion.identity);
+        float randomValue = Random.Range(0f, 1f);
+        if(randomValue > RegularProportion)
+        {
+            if(randomValue < RegularProportion + BomberProportion)
+                Instantiate(BomberPrefab, randomPosition, Quaternion.identity);
+            else
+                Instantiate(CurlingPrefab, randomPosition, Quaternion.identity);
+        }
+        else
+            Instantiate(prefabToSpawn, randomPosition, Quaternion.identity);
+
         foreach (var playerTeamManager in playerTeamManagers)
         {
             playerTeamManager.RecheckCivilians();
