001edb92175d5475076d01b9d79d413f559294b3
William Andrews <williamandrews9292@outlook.com>
2025-03-23 03:03:32 -0400

    commiting more fraud

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("AxelScene");
+        SceneManager.LoadScene("Game");
     }
 
     public void QuitGame()
