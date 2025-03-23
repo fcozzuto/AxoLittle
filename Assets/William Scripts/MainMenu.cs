e41642e740a537e8eb5858366e69aa9c45c50e97
William Andrews <williamandrews9292@outlook.com>
2025-03-23 05:05:09 -0400

    Didn't do much

--- Assets/William Scripts/MainMenu.cs
+++ Assets/William Scripts/MainMenu.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("ArenaBare");
     }
 
     public void QuitGame()
